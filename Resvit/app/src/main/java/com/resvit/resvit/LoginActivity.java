bb0e70142ffedc5881acaae8af362120f09f8e16
islammohsen <www.islammohsen@gmail.com>
2017-08-04 20:35:15 +0200

    adde login method

--- Resvit/app/src/main/java/com/resvit/resvit/LoginActivity.java
+++ Resvit/app/src/main/java/com/resvit/resvit/LoginActivity.java
@@ -1,7 +1,14 @@
 package com.resvit.resvit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.R.id.edit;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +16,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void Login(View view){
+        EditText userName = (EditText) findViewById(R.id.editText5);
+        String userText = userName.getText().toString();
+        userText = userText.toLowerCase();
+        if(userText.equals("user")) {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        }
+        else if(userText.equals("business")){
+            Intent intent = new Intent(this, BusinessActivity.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(this, "Entered else", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
