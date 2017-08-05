cb51b72ef224fa7df08be32256795e0a6530dcce
moustafa.mk <moustafa.mkms@gmail.com>
2017-08-05 02:35:43 +0200

    map activity updated

--- Resvit/app/src/main/java/com/resvit/resvit/MapsActivity.java
+++ Resvit/app/src/main/java/com/resvit/resvit/MapsActivity.java
@@ -1,20 +1,7 @@
 package com.resvit.resvit;
 
-import android.Manifest;
-import android.annotation.TargetApi;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.Network;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,12 +10,6 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
-import java.util.Map;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
@@ -56,23 +37,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        Location location;
-        double latitude;
-        double longitude;
-        boolean isGpsenabled = false;
-        boolean isNetworkenabled = false;
-        boolean cangetlocation = false;
-        private static final long MIN_DIS = 0;
-        private static final long MIN_TIME = 4000;
-        protected LocationManager locationManager;
-        LocationListener locationListener;
 
-        // Add a marker in Sydney and move the camera
-        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        latitude = location.getLatitude();
-        longitude = location.getLongitude();
-        LatLng pstatic = new LatLng(latitude, longitude);
-        mMap.addMarker(new MarkerOptions().position(pstatic).title("AUC Venture lab"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(pstatic));
+
+        LatLng auc = new LatLng(30.0254467, 31.5001621);
+        mMap.addMarker(new MarkerOptions().position(auc).title("AUC Venture lab"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(auc));
     }
 }
